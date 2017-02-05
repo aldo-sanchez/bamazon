14e03bbf0119b245f37d1418ee45bbbff254e3b6
Aldo Sanchez <Aldo Sanchez>
2017-02-04 19:54:35 -0600

    :construction: Began with inquirer and added keys_example

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,14 @@
 console.log('hello world!');
+var inquirer = require('inquirer');
+
+var promptQuestions = [{
+  type: 'input',
+  name: 'id',
+  message: 'what would you like to purchase?'
+},{
+  type: 'input',
+  name: 'quantity',
+  message: 'how many would you like to purchase?'
+}]
+
+inquirer.prompt(promptQuestions).then((answer)=>{console.log(answer.id, answer.quantity)});
